904bd91026e907195bdb4278f6b1d8c9a402bfea
Vyacheslav Bogdanov <slavabogdn@gmail.com>
2025-02-19 21:41:12 +0700

    deploy: pm2 configs

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,30 @@
+require("dotenv").config({ path: ".env.deploy" });
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REPOSITORY,
+  DEPLOY_REF = "origin/master",
+} = process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: "backend",
+      script: "./dist/app.js",
+    },
+  ],
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPOSITORY,
+      path: DEPLOY_PATH,
+      "pre-deploy-local": `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      "post-deploy":
+        "cd backend && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production",
+    },
+  },
+};
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,26 +1,30 @@
+import { NextFunction, Request, Response, Router } from "express";
+import { createUser, login } from "../controllers/users";
+import NotFoundError from "../errors/not-found-error";
+import auth from "../middlewares/auth";
 import {
-  Router, Request, Response, NextFunction,
-} from 'express';
-import userRouter from './users';
-import cardRouter from './cards';
-import auth from '../middlewares/auth';
-import NotFoundError from '../errors/not-found-error';
-import {
-  createUser, login,
-} from '../controllers/users';
-import { validateUserBody, validateAuthentication } from '../middlewares/validatons';
+  validateAuthentication,
+  validateUserBody,
+} from "../middlewares/validatons";
+import cardRouter from "./cards";
+import userRouter from "./users";
 
 const router = Router();
-router.post('/signup', validateUserBody, createUser);
-router.post('/signin', validateAuthentication, login);
+router.get("/crash_test", () => {
+  setTimeout(() => {
+    throw new Error("Сервер сейчас упадёт");
+  }, 0);
+});
+router.post("/signup", validateUserBody, createUser);
+router.post("/signin", validateAuthentication, login);
 
 // все роуты, кроме /signin и /signup, защищены авторизацией;
 router.use(auth);
-router.use('/users', userRouter);
-router.use('/cards', cardRouter);
+router.use("/users", userRouter);
+router.use("/cards", cardRouter);
 
 router.use((req: Request, res: Response, next: NextFunction) => {
-  next(new NotFoundError('Маршрут не найден'));
+  next(new NotFoundError("Маршрут не найден"));
 });
 
 export default router;
